510539c09282b0a25eb01e8e2101e9c6a301cd24
Moises Alves <moisesalves1@gmail.com>
2023-04-01 16:59:11 -0300

    QueryMultiple

--- Program.cs
+++ Program.cs
@@ -26,7 +26,8 @@ namespace DataAccess
                 // ExecuteScalar(connection);
                 // ReadView(connection);
                 // OneToOne(connection);
-                OneToMany(connection);
+                // OneToMany(connection);
+                QueryMultiple(connection);
 
 
             }
@@ -307,6 +308,24 @@ ORDER BY
             }
         }
 
+        static void QueryMultiple(SqlConnection connection)
+        {
+            var query = "SELECT * FROM [Category]; Select * FROM [COurse]";
+            using (var multi = connection.QueryMultiple(query))
+            {
+                var categories = multi.Read<Category>();
+                var courses = multi.Read<Course>();
 
+                foreach(var item in categories)
+                {
+                    Console.WriteLine(item.Title);
+                }
+
+                foreach(var item in courses)
+                {
+                    Console.WriteLine(item.Title);
+                }
+            }
+        }
     }
 }
